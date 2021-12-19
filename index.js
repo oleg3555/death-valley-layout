c39044703e59eebe4c0e007ff1d9c6d379a51bc9
oleg3555 <olegyanusik4@gmail.com>
2021-12-20 02:51:12 +0300

    rewrited js, added bem, fixed all remarks

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const images = document.querySelectorAll(".section-works-content img");
 const popup = document.querySelector('.popup');
 const popupImage = popup.querySelector('.popup-image img');
+const container = document.querySelector('.works__content');
 
-for (let i = 0; i < images.length; i++) {
-    const element = images[i];
-    element.addEventListener('click', function () {
+container.addEventListener('click', ({target}) => {
+    if (target.tagName === 'IMG') {
         popup.classList.add('popup-open');
-        popupImage.src=element.getAttribute('src');
-        popup.addEventListener('click', function (evt) {
-            if(evt.target!==popupImage) {
+        popupImage.src = target.getAttribute('src');
+        popup.addEventListener('click', (evt) => {
+            if (evt.target !== popupImage) {
                 popup.classList.remove('popup-open');
-                popup.removeEventListener('click',this)
+                popup.removeEventListener('click', this)
             }
         });
-    })
-}
+    }
+})
+
+
+document.querySelector('.contacts__form').onsubmit = function submit(e) {
+    e.preventDefault();
+    document.querySelectorAll('input')
+        .forEach((input) => console.log(input.name, ': ', input.value));
+};
+
