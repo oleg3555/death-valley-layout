2c9bbe46da3269036836895f258d37ff1d17e0e5
oleg3555 <olegyanusik4@gmail.com>
2021-12-25 21:02:45 +0300

    fixed all remarks

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const popup = document.querySelector('.popup');
-const popupImage = popup.querySelector('.popup-image img');
-const container = document.querySelector('.works__content');
+const popupImage = popup.querySelector('.popup__image img');
+const gallery = document.querySelector('.works__content');
 
-container.addEventListener('click', ({target}) => {
+gallery.addEventListener('click', ({target}) => {
     if (target.tagName === 'IMG') {
-        popup.classList.add('popup-open');
+        popup.classList.add('popup_open');
         popupImage.src = target.getAttribute('src');
-        popup.addEventListener('click', (evt) => {
-            if (evt.target !== popupImage) {
-                popup.classList.remove('popup-open');
+        popup.addEventListener('click', (event) => {
+            if (event.target !== popupImage) {
+                popup.classList.remove('popup_open');
                 popup.removeEventListener('click', this)
             }
         });
     }
 })
 
-
-document.querySelector('.contacts__form').onsubmit = function submit(e) {
-    e.preventDefault();
+const form=document.querySelector('.contacts__form');
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
     document.querySelectorAll('input')
-        .forEach((input) => console.log(input.name, ': ', input.value));
-};
+        .forEach((input) => console.log(`${input.name}: ${input.value}`));
+})
 
